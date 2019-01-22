03ac5cedbf75f68be53ef232d6041a0de5545a1a
Noel Abellera <noel.abellera@gmail.com>
2019-01-21 21:55:37 -0800

    Add Button component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Button from './components/Button/Button';
+import Row from './components/Rows/Rows';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <h1>SuperBowl 53</h1>
+        <h2>Rams</h2>
+        <Row />
+        <Button />
+        <br />
+        <h2>Patriots</h2>
+        <Row />
+        <Button />
       </div>
     );
   }
---
+++ src/components/Rows/Rows.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './Rows.css';
+
+
+const Row = (props) => {
+    return (
+        <div id="Row">
+            <li class="box"></li>
+            <li class="box"></li>
+            <li class="box"></li>
+            <li class="box"></li>
+            <li class="box"></li>
+            <li class="box"></li>
+            <li class="box"></li>
+            <li class="box"></li>
+            <li class="box"></li>
+            <li class="box"></li>
+        </div>
+    )
+}
+
+export default Row;
