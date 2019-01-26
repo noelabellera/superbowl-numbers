08c2b40bbcbd152ae13443abbf3da7af5ef07e1d
Noel Abellera <noel.abellera@gmail.com>
2019-01-25 21:35:11 -0800

    display randomized numbers on screen

--- src/App.js
+++ src/App.js
@@ -10,13 +10,14 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      arr: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+      arr: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+      msg: "Hello"
     }
   }
 
 
   randomizeNumbers = () => {
-    console.log(this.state.arr)
+    // console.log(this.state.arr)
     this.state.arr.sort(() => Math.random() - 0.5);
     console.log(this.state.arr);
     return this.state.arr;
@@ -24,21 +25,28 @@ class App extends Component {
 
   updateState = () => {
     this.setState({
-      arr: this.randomizeNumbers()
+      arr: this.randomizeNumbers(),
+      msg: "Hello World"
+    }, () => {
+      console.log(this.state.msg);
     })
+
   }
 
+  
   render() {
     return (
       <div>
         <h1>SuperBowl 53</h1>
         <h2>Rams</h2>
-        <Row />
+        <Row nums={this.state.arr}/>
         <Button updateState={this.updateState}/>
         <br />
         <h2>Patriots</h2>
-        <Row />
-        <Button />
+        <Row nums={this.state.arr}/>
+        <Button updateState={this.updateState}/>
+        <br/>
+        <h2>{this.state.msg}</h2>
       </div>
     );
   }
--- src/components/Rows/Rows.jsx
+++ src/components/Rows/Rows.jsx
@@ -3,18 +3,15 @@ import './Rows.css';
 
 
 const Row = (props) => {
+    // const nums = [0,1,2,3,4,5,6,7,8,9]
+    console.log(props);
     return (
         <div id="Row">
-            <li className="box"></li>
-            <li className="box"></li>
-            <li className="box"></li>
-            <li className="box"></li>
-            <li className="box"></li>
-            <li className="box"></li>
-            <li className="box"></li>
-            <li className="box"></li>
-            <li className="box"></li>
-            <li className="box"></li>
+            {
+                props.nums.map(n => {
+                    return <li className="box" key={`number-${n}`}>{n}</li>
+                })
+            }
         </div>
     )
 }
